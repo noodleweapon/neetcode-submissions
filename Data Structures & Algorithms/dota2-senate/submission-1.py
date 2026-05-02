757f3c8b64652b16e999dee1f14e0f94efd92078
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-05-01 22:41:57 -0500

    Add: dota2-senate - submission-1

---
+++ Data Structures & Algorithms/dota2-senate/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        D_kills = 0
+        R_kills = 0
+        last_kill = 'Dire' if senate[0] == 'D' else 'Radiant'
+        ongoing = True
+        alive = [True] * len(senate)
+        while ongoing:
+            ongoing = False
+            for i, c in enumerate(senate):
+                if not alive[i]:
+                    continue
+                if c == 'R':
+                    if D_kills > 0:
+                        D_kills -= 1
+                        alive[i] = False
+                        last_kill = 'Dire'
+                        ongoing = True
+                    else:
+                        R_kills += 1
+                elif c == 'D':
+                    if R_kills > 0:
+                        R_kills -= 1
+                        alive[i] = False
+                        last_kill = 'Radiant'
+                        ongoing = True
+                    else:
+                        D_kills += 1
+
+        return last_kill
