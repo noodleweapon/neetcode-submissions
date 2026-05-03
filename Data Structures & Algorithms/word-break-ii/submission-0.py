933e41fae20d62846f014a9aaa0faf42fa09a9ec
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-05-03 10:26:12 -0500

    Add: word-break-ii - submission-0

---
+++ Data Structures & Algorithms/word-break-ii/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        res = []
+        def dfs(i, words):
+            if i == len(s):
+                res.append(" ".join(words))
+                return
+            
+            for word in wordDict:
+                j = i + len(word)
+                if j > len(s):
+                    continue
+                if s[i:j] != word:
+                    continue
+                words.append(word)
+                dfs(j, words)
+                words.pop()
+
+        dfs(0, [])
+        return res
