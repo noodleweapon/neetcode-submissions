3956f3057e3c247f713e0ec8ed2f3ff9fd322bd8
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-05-04 23:27:34 -0500

    Add: minimum-height-trees - submission-3

---
+++ Data Structures & Algorithms/minimum-height-trees/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        if n == 1:
+            return [0]
+        g = [set() for _ in range(n)]
+        for a, b in edges:
+            g[a].add(b)
+            g[b].add(a)
+        
+        leaves = [i for i in range(n) if len(g[i]) == 1]
+        nodes = n
+        
+        while nodes > 2:
+            new_leaves = []
+            nodes -= len(leaves)
+            
+            for u in leaves:
+                v = g[u].pop() # since it only has one neighbor.
+                g[v].remove(u)
+                if len(g[v]) == 1:
+                    new_leaves.append(v)
+            
+            leaves = new_leaves
+        
+        return leaves
