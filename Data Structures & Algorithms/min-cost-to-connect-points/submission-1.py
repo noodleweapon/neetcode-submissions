e454085396de5176158ab16885193788d7c36a17
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-05-06 13:42:15 -0500

    Add: min-cost-to-connect-points - submission-1

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        par = [i for i in range(n)]
+        rank = [1] * n
+    
+        def find(v):
+            if par[v] != v:
+                par[v] = find(par[v])
+            return par[v]
+
+        def union(a, b):
+            A, B = find(a), find(b)
+            if A == B:
+                return False
+            if rank[A] > rank[B]:
+                par[B] = A
+                rank[A] += rank[B]
+            else:
+                par[A] = B
+                rank[B] += rank[A]
+            return True
+        
+        edges = []
+        for i1, p1 in enumerate(points):
+            for i2, p2 in enumerate(points):
+                if i1 >= i2:
+                    continue
+                dist = abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+                heapq.heappush(edges, (dist, i1, i2))
+        
+        min_cost = 0
+        while edges:
+            (d, i1, i2) = heapq.heappop(edges)
+            if not union(i1, i2):
+                continue
+            min_cost += d
+        return min_cost
+
