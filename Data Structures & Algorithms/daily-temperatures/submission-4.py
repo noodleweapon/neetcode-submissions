c3e5dcd9a5a54d3fb1e9a3829cebae26a89ba849
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-06-07 16:15:12 -0400

    Add: daily-temperatures - submission-4

---
+++ Data Structures & Algorithms/daily-temperatures/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # 30,38,30,36,35,40,28
+        # 1,4,1,2,1,0,0
+
+        n = len(temperatures)
+        s = []
+        res = [0] * n
+        for i in reversed(range(n)):
+            T = temperatures[i]
+            while s and temperatures[s[-1]] <= T:
+                s.pop()
+            if s:
+                res[i] = s[-1] - i
+            s.append(i)
+        return res
