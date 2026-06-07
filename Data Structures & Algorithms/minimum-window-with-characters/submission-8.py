dbbe0ef78722e7420af50463dc7424fac9864e97
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-06-07 14:43:17 -0400

    Add: minimum-window-with-characters - submission-8

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-8.py
@@ -0,0 +1,33 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        d = Counter(t)
+        res = ""
+        M = float("inf")
+        l = 0
+        S = set(list(t))
+        for r in range(len(s)):
+            d[s[r]] -= 1
+            while l < r and d[s[l]] < 0:
+                d[s[l]] += 1
+                l += 1
+
+            if d[s[r]] <= 0:
+                is_valid = True
+                for char in S:
+                    if d[char] > 0:
+                        is_valid = False
+                        break
+                if is_valid:
+                    m = r - l + 1
+                    if m < M:
+                        M = m
+                        res = s[l:r + 1]
+
+        return res
+
+        # OUZODYXAZV
+        # XYZ
+
+
+        # OUZODYXAZZZV
+        # XYZZZ
