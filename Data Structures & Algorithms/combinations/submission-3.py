f975cbb9d6554fe42a0f80c78aeaf26f4453b81f
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-06-09 20:39:01 -0700

    Add: combinations - submission-3

---
+++ Data Structures & Algorithms/combinations/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        elems = list(range(1, n + 1))
+        res = []
+
+        def rec(i, arr):
+            if i == n:
+                if len(arr) == k:
+                    res.append(arr.copy())
+                return
+            
+            rec(i + 1, arr)
+            elems_missing = k - len(arr)
+            opportunities = n - i
+            if elems_missing > opportunities:
+                return
+            arr.append(elems[i])
+            rec(i + 1, arr)
+            arr.pop()
+
+        rec(0, [])
+        return res
+
+
+        # n = 1,2,3,4,5
+        # k = 3
