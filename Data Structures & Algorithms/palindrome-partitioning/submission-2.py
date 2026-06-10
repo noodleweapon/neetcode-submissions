1c10868f344fd7025b2ac8f5ea071a54af85302d
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-06-09 21:06:08 -0700

    Add: palindrome-partitioning - submission-2

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        n = len(s)
+        res = []
+
+        def is_pal(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
+        def rec(i, arr):
+            if i == n:
+                res.append(arr.copy())
+                return
+            for j in range(i, n):
+                if is_pal(i, j):
+                    arr.append(s[i:j+1])
+                    rec(j + 1, arr)
+                    arr.pop()
+
+        rec(0, [])
+        return res
+
+
+
+        # Input: s = "aab"
+        # Output: [["a","a","b"],["aa","b"]]
