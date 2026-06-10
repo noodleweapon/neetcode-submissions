f60d1d2c58df1b3bdb89af08a0b4751fd6bd65ce
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-06-10 09:38:20 -0700

    Add: word-break-ii - submission-8

---
+++ Data Structures & Algorithms/word-break-ii/submission-8.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordBreak(self, s: str, words: List[str]) -> List[str]:
+        words = set(words)
+        ls = set(map(len, words))
+        def rec(i):
+            if i == len(s):
+                return ["<>"]
+            res = []
+            for l in ls:
+                j = i + l
+                if j > len(s):
+                    continue
+                word = s[i:j]
+                if word not in words:
+                    continue
+                for tail in rec(j):
+                    if tail == "<>":
+                        res.append(word)
+                    else:
+                        res.append(word + " " + tail)
+            return res
+        
+        return rec(0)
