299462930a1bef83c036aadd93bc9e38c8fb3dc3
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-06-12 21:29:22 -0700

    Add: accounts-merge - submission-4

---
+++ Data Structures & Algorithms/accounts-merge/submission-4.py
@@ -0,0 +1,49 @@
+class UF:
+    def __init__(self, n, names, emails):
+        self.rank = [1] * n
+        self.par = [i for i in range(n)]
+        self.names = names
+        self.emails = emails
+    
+    def find(self, i):
+        if self.par[i] != i:
+            self.par[i] = self.find(self.par[i])
+        return self.par[i]
+        
+    def union(self, a, b):
+        A, B = self.find(a), self.find(b)
+        if A == B:
+            return False
+        if self.rank[A] > self.rank[B]:
+            self.rank[A] += self.rank[B]
+            self.rank[B] = 0
+            self.emails[A] |= self.emails[B]
+            self.emails[B] = set()
+            self.par[B] = A
+        else:
+            self.rank[B] += self.rank[A]
+            self.rank[A] = 0
+            self.emails[B] |= self.emails[A]
+            self.emails[A] = set()
+            self.par[A] = B
+        return True
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        n = len(accounts)
+        names = list(map(lambda x: x[0], accounts))
+        emails = list(map(lambda x: set(x[1:]), accounts))
+        uf = UF(n, names, emails)
+        emailDict = {}
+        for i in range(n):
+            for email in accounts[i][1:]:
+                if email in emailDict:
+                    uf.union(emailDict[email], i)
+                emailDict[email] = i
+
+        res = []
+        for i in range(n):
+            if uf.rank[i] > 0:
+                res.append([uf.names[i]] + list(sorted(uf.emails[i])))
+
+        return res
