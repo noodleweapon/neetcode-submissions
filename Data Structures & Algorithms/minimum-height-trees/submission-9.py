c53e6a6e397f082e32054d60ac6ca63a7323beee
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-06-13 17:33:20 -0700

    Add: minimum-height-trees - submission-9

---
+++ Data Structures & Algorithms/minimum-height-trees/submission-9.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        g = [[] for _ in range(n)]
+        for a, b in edges:
+            g[a].append(b)
+            g[b].append(a)
+
+        def get_furthest(start):
+            def dfs(u, p):
+                longestPath = [u]
+                for v in g[u]:
+                    if v == p:
+                        continue
+                    path = dfs(v, u)
+                    path.append(u)
+                    if len(path) > len(longestPath):
+                        longestPath = path
+                return longestPath
+            return dfs(start, -1)
+        
+        path1 = get_furthest(0)
+        path2 = get_furthest(path1[0])
+        l, r = 0, len(path2) - 1
+        while l + 1 <= r - 1:
+            l += 1
+            r -= 1
+        
+        return path2[l:r+1]
