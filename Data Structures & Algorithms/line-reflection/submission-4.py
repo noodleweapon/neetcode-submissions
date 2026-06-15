522b64fc0e60e4dd669dc687ece3daf57868677a
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-06-14 19:04:25 -0700

    Add: line-reflection - submission-4

---
+++ Data Structures & Algorithms/line-reflection/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isReflected(self, points: List[List[int]]) -> bool:
+        points = list(map(tuple, points))
+        members = set(points)
+        min_x = min([x for x, y in points])
+        max_x = max([x for x, y in points])
+        const = min_x + max_x
+        for x, y in points:
+            if (const - x, y) not in members:
+                return False
+        return True
