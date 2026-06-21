676752ab2a9cae9b8474cbad0e5e3be26ecb774a
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-06-21 12:53:33 -0700

    Add: binary-tree-upside-down - submission-6

---
+++ Data Structures & Algorithms/binary-tree-upside-down/submission-6.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def upsideDownBinaryTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        head = None
+        def dfs(node, parent):
+            nonlocal head
+            if node.left:
+                dfs(node.left, node)
+
+                l, r = node.left, node.right
+                l.left, l.right = r, node
+                node.left, node.right = None, None
+
+            if not head:
+                head = node
+
+        dfs(root, None)
+        return head
