44bae596a574bf5756b6c0dfc144e28789e222e3
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-06-24 17:58:38 -0700

    Add: count-strictly-increasing-subarrays - submission-1

---
+++ Data Structures & Algorithms/count-strictly-increasing-subarrays/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countSubarrays(self, nums: List[int]) -> int:
+        def ways(d):
+            return d * (d + 1) // 2
+
+        res = 0
+        length = 1
+        for i in range(1, len(nums)):
+            if nums[i - 1] < nums[i]:
+                length += 1
+            else:
+                res += ways(length)
+                length = 1
+        if length > 0:
+            res += ways(length)
+        return res
+        
+        # 1,3,5,4,4,6
+        # 2 --> 2 + 1 = 3 = 2 * (2 + 1) / 2 = 3
+        # 3 --> 3 + 2 + 1 = 6, 3 * (3 + 1) / 2 = 6
