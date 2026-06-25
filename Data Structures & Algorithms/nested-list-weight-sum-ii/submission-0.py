23dd689e6ee3c04a94415261e53302a148b815fe
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-06-24 22:53:13 -0700

    Add: nested-list-weight-sum-ii - submission-0

---
+++ Data Structures & Algorithms/nested-list-weight-sum-ii/submission-0.py
@@ -0,0 +1,69 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        q = deque()
+        for elem in nestedList:
+            q.append((elem, 0))
+        
+        vals = []
+        depths = []
+        while q:
+            elem, depth = q.popleft()
+            
+            if elem.isInteger():
+                vals.append(elem.getInteger())
+                depths.append(depth)
+            else:
+                for elem in elem.getList():
+                    q.append((elem, depth + 1))
+        
+        res = 0
+        maxD = max(depths)
+        for w, d in zip(vals, depths):
+            res += (maxD + 1 - d) * w
+        return res
+        
+
+
