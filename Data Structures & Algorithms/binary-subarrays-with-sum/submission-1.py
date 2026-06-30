04929da7f9d1f9c205886ee4a74bcf39de058dd6
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-06-29 23:20:32 -0700

    Add: binary-subarrays-with-sum - submission-1

---
+++ Data Structures & Algorithms/binary-subarrays-with-sum/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        if goal == 0:
+            res = 0
+            length = 0
+            for num in nums:
+                if num == 1:
+                    length = 0
+                    continue
+                length += 1
+                res += length
+            return res
+
+        #     [1,0,1,0,1]
+        #   [0,1,1,2,2,3]
+        d = defaultdict(int)
+        d[0] += 1
+        res = accum = 0
+        for num in nums:
+            accum += num
+            res += d[accum - goal]
+            d[accum] += 1
+        return res
