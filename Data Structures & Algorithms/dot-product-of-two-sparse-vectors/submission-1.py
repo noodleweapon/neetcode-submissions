07534fc96d08e76bdd4801c14c400d1bd64c738a
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-06-30 22:59:32 -0700

    Add: dot-product-of-two-sparse-vectors - submission-1

---
+++ Data Structures & Algorithms/dot-product-of-two-sparse-vectors/submission-1.py
@@ -0,0 +1,23 @@
+class SparseVector:
+    def __init__(self, nums: List[int]):
+        self.inds = set()
+        self.nums = nums
+        self.n = len(nums)
+        for i in range(self.n):
+            if nums[i] != 0:
+                self.inds.add(i)
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        if len(self.inds) > len(vec.inds):
+            return vec.dotProduct(self)
+        res = 0
+        for ind in self.inds:
+            res += self.nums[ind] * vec.nums[ind]
+        return res
+
+
+# Your SparseVector object will be instantiated and called as such:
+# v1 = SparseVector(nums1)
+# v2 = SparseVector(nums2)
+# ans = v1.dotProduct(v2)
