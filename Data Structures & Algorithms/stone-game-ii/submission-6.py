fe1bea6d55bb51c2a44f4cb883a51ca4b9d05671
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-07-04 19:55:21 -0700

    Add: stone-game-ii - submission-6

---
+++ Data Structures & Algorithms/stone-game-ii/submission-6.py
@@ -0,0 +1,30 @@
+class Solution:
+    def stoneGameII(self, piles: List[int]) -> int:
+        n = len(piles)
+        dp = {}
+        def rec(i, M, alice):
+            if i >= n:
+                return 0
+            if (i, M, alice) in dp:
+                return dp[(i, M, alice)]
+            if alice:
+                res = float("-inf")
+                for X in range(1, 2 * M + 1):
+                    call = sum(piles[i:i+X]) + rec(i + X, max(M, X), not alice)
+                    res = max(res, call)
+            else:
+                # Bob should minimize Alice’s future total, not “Alice minus Bob’s stones”.
+                # BAD:
+                    # val = rec(i + X, max(M, X), not alice)
+                    # minimize = -sum(piles[i:i+X]) + val
+                res = float("inf")
+                for X in range(1, 2 * M + 1):
+                    res = min(res, rec(i + X, max(M, X), not alice))
+            dp[(i, M, alice)] = res
+            return res
+
+        return rec(0, 1, True)
+
+        # [3,1,2,5,7]
+        # 1 <= x <= 2M
+
