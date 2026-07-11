d0b0559bf96f9b7713623eb0f8eaaea540164b2e
Jason Yeon <83441512+noodleweapon@users.noreply.github.com>
2026-07-10 23:19:48 -0700

    Add: my-calendar-i - submission-0

---
+++ Data Structures & Algorithms/my-calendar-i/submission-0.py
@@ -0,0 +1,35 @@
+class MyCalendar:
+    
+    def __init__(self):
+        self.free = [(0, float("inf"))]
+
+    def book(self, startTime: int, endTime: int) -> bool:
+        l, r = 0, len(self.free) - 1
+        while l < r:
+            m = (l + r + 1) // 2
+            if self.free[m][0] <= startTime:
+                # MISTAKE: I had it reversed.
+                l = m
+            else:
+                r = m - 1
+        
+        L, R = self.free[l]
+        print(L, startTime, endTime, R)
+        if not (L <= startTime and endTime <= R):
+            return False
+
+        self.free.pop(l)
+        if L != startTime:
+            self.free.append((L, startTime))
+        if R != endTime:
+            self.free.append((endTime, R))
+        self.free.sort()
+        print(self.free)
+        return True
+
+        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(startTime,endTime)
